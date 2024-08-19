bd2b643cae30b186d4b054f7e63f1f4d91c716cc
Bruno Manfrinato <brunomanfrinato@Brunos-MacBook-Pro.local>
2024-08-19 20:43:41 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Integrate Whatsapp API with Node.js
+
+Integrate Whatsapp API with Node.js. Using Axios.
+
+### Version: 1.0.0
+
+### Usage
+
+```sh
+$ npm install
+```
+
+```sh
+$ node index
+```
---
+++ index.js
@@ -0,0 +1,118 @@
+require('dotenv').config()
+const axios = require('axios')
+const FormData = require('form-data')
+const fs = require('fs')
+
+async function sendTemplateMessage() {
+    const response = await axios({
+        url: 'https://graph.facebook.com/v20.0/397648603426578/messages',
+        method: 'post',
+        headers: {
+            'Authorization': `Bearer ${process.env.WHATSAPP_TOKEN}`,
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify({
+            messaging_product: 'whatsapp',
+            to: 'phone_number',
+            type: 'template',
+            template:{
+                name: 'discount',
+                language: {
+                    code: 'en_US'
+                },
+                components: [
+                    {
+                        type: 'header',
+                        parameters: [
+                            {
+                                type: 'text',
+                                text: 'John Doe'
+                            }
+                        ]
+                    },
+                    {
+                        type: 'body',
+                        parameters: [
+                            {
+                                type: 'text',
+                                text: '50'
+                            }
+                        ]
+                    }
+                ]
+            }
+        })
+    })
+
+    console.log(response.data)
+}
+
+async function sendTextMessage() {
+    const response = await axios({
+        url: 'https://graph.facebook.com/v20.0/397648603426578/messages',
+        method: 'post',
+        headers: {
+            'Authorization': `Bearer ${process.env.WHATSAPP_TOKEN}`,
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify({
+            messaging_product: 'whatsapp',
+            to: 'phone_number',
+            type: 'text',
+            text:{
+                body: 'This is a text message'
+            }
+        })
+    })
+
+    console.log(response.data) 
+}
+
+async function sendMediaMessage() {
+    const response = await axios({
+        url: 'https://graph.facebook.com/v20.0/397648603426578/messages',
+        method: 'post',
+        headers: {
+            'Authorization': `Bearer ${process.env.WHATSAPP_TOKEN}`,
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify({
+            messaging_product: 'whatsapp',
+            to: 'phone_number',
+            type: 'image',
+            image:{
+                //link: 'https://dummyimage.com/600x400/000/fff.png&text=manfra.io',
+                id: '512126264622813',
+                caption: 'This is a media message'
+            }
+        })
+    })
+
+    console.log(response.data)    
+}
+
+async function uploadImage() {
+    const data = new FormData()
+    data.append('messaging_product', 'whatsapp')
+    data.append('file', fs.createReadStream(process.cwd() + '/logo.png'), { contentType: 'image/png' })
+    data.append('type', 'image/png')
+
+    const response = await axios({
+        url: 'https://graph.facebook.com/v20.0/397648603426578/media',
+        method: 'post',
+        headers: {
+            'Authorization': `Bearer ${process.env.WHATSAPP_TOKEN}`
+        },
+        data: data
+    })
+
+    console.log(response.data)     
+}
+
+// sendTemplateMessage()
+
+// sendTextMessage()
+
+// sendMediaMessage()
+
+// uploadImage()
